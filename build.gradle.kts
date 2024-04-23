694e2e8f55388bde7feae508f50999d818d97104
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-04-23 15:17:22 +0700

    fix error

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.5"
     jacoco
-    id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
 }
 
