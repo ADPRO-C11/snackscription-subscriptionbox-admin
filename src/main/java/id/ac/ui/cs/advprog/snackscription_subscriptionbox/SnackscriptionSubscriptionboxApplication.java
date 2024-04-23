40956975ea024e77ab24e6eae528b19904012a6a
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-04-23 13:39:28 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/snackscription_subscriptionbox/SnackscriptionSubscriptionboxApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.snackscription_subscriptionbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackscriptionSubscriptionboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnackscriptionSubscriptionboxApplication.class, args);
+    }
+
+}
