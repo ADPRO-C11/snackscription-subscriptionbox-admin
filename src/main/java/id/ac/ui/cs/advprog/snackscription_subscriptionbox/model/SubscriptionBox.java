ca0fa2675c0a9f87d22063703285fdc87aa0f05d
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-04-24 19:26:19 +0700

    [RED] Make Tests for Subscription Box

--- src/main/java/id/ac/ui/cs/advprog/snackscription_subscriptionbox/model/SubscriptionBox.java
+++ src/main/java/id/ac/ui/cs/advprog/snackscription_subscriptionbox/model/SubscriptionBox.java
@@ -8,11 +8,5 @@ import lombok.Getter;
 import lombok.Setter;
 @Getter@Setter
 public class SubscriptionBox {
-    private String id;
-    private String name;
-    private String category;
-    private String photo; //Box Thumbnail Address from static folder
-    private List<Integer> price; // Prices according to subscription length (1 month/3 month/ 6 month)
-    private List<Items> items; // Item Model
-    private Date dateCreated;
+
 }
