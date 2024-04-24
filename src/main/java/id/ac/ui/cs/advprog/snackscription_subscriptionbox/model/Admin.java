8f8c6ce8a884ad392f3b617e9f58beb2aa81e0d4
sitaamirasyarifah <sitaamira86@gmail.com>
2024-04-24 20:02:57 +0700

    Add models, repo, service, controller

---
+++ src/main/java/id/ac/ui/cs/advprog/snackscription_subscriptionbox/model/Admin.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.snackscription_subscriptionbox.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Admin {
+    private String username;
+    private String password;
+    private int itemsQuantity;
+}
