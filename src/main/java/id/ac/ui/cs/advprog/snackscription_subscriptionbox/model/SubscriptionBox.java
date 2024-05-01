5a7fbbaac962e4deaecb2388d1daf95aae5cbda7
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-05-01 20:53:14 +0700

    [DEPLOYMENT] imported jakarta.persistence.*, havent implement

--- src/main/java/id/ac/ui/cs/advprog/snackscription_subscriptionbox/model/SubscriptionBox.java
+++ src/main/java/id/ac/ui/cs/advprog/snackscription_subscriptionbox/model/SubscriptionBox.java
@@ -1,16 +1,21 @@
 package id.ac.ui.cs.advprog.snackscription_subscriptionbox.model;
 
+
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
 
+
 @Getter @Setter
 public class SubscriptionBox {
+
     String id;
     String name;
     String type;
     int price;
     List<Items> items;
+
 
 }
