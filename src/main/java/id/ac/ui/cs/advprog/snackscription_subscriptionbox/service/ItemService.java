76b767f91386447d94895789fcdd8fc8f46222d4
sitaamirasyarifah <sitaamira86@gmail.com>
2024-05-25 12:32:30 +0700

    [RED] Added test for ItemServiceImpl.java

--- src/main/java/id/ac/ui/cs/advprog/snackscription_subscriptionbox/service/ItemService.java
+++ src/main/java/id/ac/ui/cs/advprog/snackscription_subscriptionbox/service/ItemService.java
@@ -1,5 +1,13 @@
 package id.ac.ui.cs.advprog.snackscription_subscriptionbox.service;
 
-public class ItemService {
+import id.ac.ui.cs.advprog.snackscription_subscriptionbox.model.Item;
+import java.util.List;
+
+public interface ItemService {
+    Item createItem(String id, String name, int quantity);
+    Item getItemById(String id);
+    Item deleteItem(String id);
+    Item editItem(String id, String name, int quantity);
+    List<Item> getAllItems();
 
 }
